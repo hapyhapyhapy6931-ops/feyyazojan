ca06d400660fb574323d91d3c777ef5c63b318cb
hapyhapyhapy6931-ops <hapyhapyhapy6931@gmail.com>
2026-05-28 04:12:20 +0300

    Add mail configuration

---
+++ config/mail.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'default' => env('MAIL_MAILER', 'smtp'),
+    'mailers' => [
+        'smtp' => [
+            'transport' => 'smtp',
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'auth_mode' => null,
+        ],
+        'ses' => [
+            'transport' => 'ses',
+        ],
+        'mailgun' => [
+            'transport' => 'mailgun',
+        ],
+        'postmark' => [
+            'transport' => 'postmark',
+        ],
+        'sendmail' => [
+            'transport' => 'sendmail',
+            'path' => '/usr/sbin/sendmail -t -i',
+        ],
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+        'array' => [
+            'transport' => 'array',
+        ],
+    ],
+    'from' => [
+        'name' => env('MAIL_FROM_NAME', 'Support Ticket Platform'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@example.com'),
+    ],
+    'markdown' => [
+        'theme' => 'default',
+        'paths' => [
+            resource_path('views/vendor/mail'),
+        ],
+    ],
+];
